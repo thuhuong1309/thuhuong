8573f39c7110740e8b3e382fdd54f8d49ae5a1c4
thuhuong1309 <114062133+thuhuong1309@users.noreply.github.com>
2022-09-21 15:32:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Đăng Đắng';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100021923201390';
+const eventName = 'Hòm Thư bí mật';
+const titleCard = 'Gửi Chị ';
+const contentCard = 'Chào chị! người con gái hơi cọc cằn,thẳng tính đương nhiên là rất dễ thương^^ Dù là người em quen muộn nhất nhưng giờ lại là người mà em quý nhất ^_^ ít nói chuyện Ít đi chơi với nhau Ít "Tình Cảm" nhưng lại buồn nếu không thấy mặt chị:( Sắp tới không được nhìn thấy mẹt chị chắc buồn lắm đây(ai sẽ nấu cơm cho em ăn đây):( Mau chóng khỏi bệnh rồi lại vi vu nhá chị^^Nhớ là chị luôn có 1 thằng em vừa cứt vừa tệ luôn luôn tốt với chị trong mọi hoàn cảnh đó nha';
+
+
+const giftImage = '308030270_455173563321270_1917281185596997248_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
